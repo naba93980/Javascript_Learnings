745536d6bb28ea458bec9bced3054455ba26dd44
naba93980 <72063612+naba93980@users.noreply.github.com>
2022-05-08 04:16:52 +0530

    updated on 8/5/22 at 4:15am

--- JS_Thapa_technical/16.Access document elements/index.js
+++ JS_Thapa_technical/16.Access document elements/index.js
@@ -1,6 +1,56 @@
+
 /*
 Document.getElementById()
 
     Returns an object reference to the identified element. (An object reference is information on how to find a particular object. The object is a chunk of main memory; a reference to the object is a way to get to that chunk of memory. The variable str does not actually contain the object, but contains information about where the object is. Objects are created while a program is running. Each object has a unique object reference, which is used to find it. When an object reference is assigned to a variable, then that variable says how to find that object.)
+  
+Document.getElementsByClass()  
+
+*/
+function fun(event)
+{
+    console.log( event );
+    
+    document.getElementById("para1").innerHTML = "my name is nabajyoti modak";
+    
+        
+}
+function fun2()
+    {
+    console.log(document.getElementsByClassName("para2"));
+    Array.from(document.getElementsByClassName("para2")).forEach((element)=>(element.innerHTML='MODAK'))
+    
+}
+function fun3()
+    {
+    console.log(document.getElementsByTagName("p"));
+    Array.from(document.getElementsByTagName("p")).forEach((element)=>(element.innerHTML='MODAK'))
+    
+}
+function fun4()
+    {
+    // console.log(document.getElementsByName("male"));
+    Array.from(document.getElementsByName("gender")).forEach((element) => (console.log(element)));    
+}
+//querySelector() returns first matching value whereas querySelectorAll() will return all the elements.
+function fun5()
+{
+    document.querySelector("#para1").innerHTML = "naabababjytoitoi";
+}
+function fun6()
+{
+    console.log(document.querySelectorAll("p"));
+    //  Array.from(document.querySelectorAll(".para2")).forEach((element)=>(element.innerHTML='MODAK'))
+     Array.from(document.querySelectorAll("p")).forEach((element)=>(element.innerHTML='MODAK'))
+    
+}
+/*
+getElementsByClassName will return a live HTMLCollection, possibly of length 0 if no matching elements are found
+The method querySelector also returns a single element, and querySelectorAll returns an iterable collection.
+If neither element is found, the querySelector method returns null 
+
+getElementbyId will return an Element object or null if no element with the ID is found.
+
+getElementbyId() is faster than querySelector()
 
 */
---
+++ JS_Thapa_technical/17.Events/index.js
@@ -0,0 +1,71 @@
+// In the case of the Web, events are fired inside the browser window, and tend to be attached to a specific item that resides in it.
+// This might be a single element, a set of elements, the HTML document loaded in the current tab, or the entire browser window.
+
+//To react to an event, you attach an {event handler} to it. This is a block of code (usually a JavaScript function that you as a
+//programmer create that runs when the event fires.When such a block of code is defined to run in response to an event,
+//we say we are {registering an event handler}.Note: Event handlers are sometimes called {event listeners} —
+//they are pretty much interchangeable for our purposes, although strictly speaking, they work together.
+//The {listener listens out for the event happening,} and the {handler is the code that is run in response} to it happening.
+
+//An event called 'click'  fires when the user clicks the button. Objects that can fire events have an addEventListener() method,
+//that takes at least two arguments: the name of the event and a function to handle the event
+
+//By convention, Javascript objects that fire events have a corresponding "onevent" properties (named by prefixing "on" to the name of the event).
+//These properties are called to run associated handler code when the event is fired
+
+
+// The Node.js event model relies on listeners to listen for events and emitters to emit events periodically — it doesn't sound that
+//different, but the code is quite different, making use of functions like on() to register an event listener, and once() to register
+//an event listener that unregisters after it has run once.
+
+
+//https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Building_blocks/Events
+
+/*
+Registering event handlers
+
+There are two recommended approaches for registering handlers. Event handler code can be made to run when an event is triggered by assigning 
+it to the target element's corresponding onevent property, or by registering the handler as a listener for the element using the addEventListener() 
+method. In either case the handler will receive an object that conforms to the Event interface (or a derived interface). The main difference is that 
+multiple event handlers can be added (or removed) using the event listener methods.
+*/
+// elemenst can use event(eg. click) and has onevent properties(onclick) for event handling.
+
+//In either case the handler will receive an object that conforms to the Event interface (or a derived interface).
+
+/*
+4 ways of writing Events in javascript
+
+1. using inline events alert();
+2. by calling a function and assining it to element.onevent property
+3. using inline events(html onclick property)
+4. using addeventlistener function
+*/
+var fun2 = () => {
+    alert("this is 2nd way");
+}
+//console.log( h ); //Cannot access 'h' before initialization, h remains in temporal dead zone in script object  until it gets initialized., js execution stops as it throws error here
+//let h=6;
+
+var btn3 = document.getElementById("btn3");
+btn3.onclick = () => alert("this is 3rd way");
+
+var btn4 = document.querySelector("#btn4");
+btn4.addEventListener("mouseup", (e) => {
+    // alert("this is 4th way");
+    // console.warn( e,e.target,e.type );
+    window.document.documentElement.style.backgroundColor = "white";
+})
+btn4.addEventListener("click", (e) => {  //click event is both mousedown then mouseup
+    alert("this is 4th way");
+    console.warn( e,e.target,e.type );
+})
+btn4.addEventListener("mousedown", (e) => {
+    // alert("this is 4th way");
+    // console.warn( e,e.target,e.type );
+    window.document.documentElement.style.backgroundColor = "red";
+})
+
+
+//you can call addEventListener('click', handler) on an element multiple times, with different functions specified in the second argument
+//With event handler properties, you can't add more than one handler for a single event
