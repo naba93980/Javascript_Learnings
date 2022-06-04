2a983874d5e95101e552e6aac60e1135066582f8
naba93980 <72063612+naba93980@users.noreply.github.com>
2022-06-05 01:08:35 +0530

    updated on 5/6/22 at 1 am

--- JS_other_concepts/4.Asynchronous JS/Callback(hell)/index.js
+++ JS_other_concepts/4.Asynchronous JS/Callback(hell)/index.js
@@ -1,4 +1,4 @@
-//synchronous callbacks
+//synchronous onSuccessCallbacks
 
 /*console.log("start");
 const items=[1,2,3,4,5,6,7];
@@ -7,44 +7,45 @@ items.forEach((element)=>{
 })
 console.log("start");
 */
-//asynchronous callbacks
+//asynchronous onSuccessCallbacks
 
 console.log("start");
 
-function user(email,password,callback){
+function user(email,password,onSuccessCallback){
     setTimeout(()=>{
         // return ({"usermail":email});
         // console.log(email);
-        callback({"user":email,"password":password});
+        onSuccessCallback({"user":email,"password":password});
     },2000);
 }
-function getUserVideos(email,callback){
+
+
+function getUserVideos(email,onSuccessCallback){
     setTimeout(()=>{
-        callback(["video","video2","video3"]);
+        onSuccessCallback(["video","video2","video3"]);
     },4000)
 }
-function getvideoDetails(video,callback){
+
+
+function getvideoDetails(video,onSuccessCallback){
     setTimeout(()=>{
-        callback({"name":"dhinkachika","duration":"11:23 minutes"})
+        onSuccessCallback({"name":"dhinkachika","duration":"11:23 minutes"})
     },5000)
 }
+
+
 user("naba93980",12345,(userDetail)=>{
     console.log(userDetail);
-    Object.keys(userDetail).forEach(element => {
-        console.log(element);
-    });
-    Object.values(userDetail).forEach(element => {
-        console.log(element);
-    });
+    
     getUserVideos(userDetail.user,(videos)=>
     {
-        videos.forEach((element)=>{
-            console.log(element);
-        })
+        console.log(videos);
+        
         getvideoDetails(videos[0],(videoDetails)=>{
             console.log(videoDetails);
         })
     });
 });
 console.log("end");
+//callbacks calling a function and passing another callback as parameter, basically callbacks passing callbacks is callback hell.
 
--- JS_other_concepts/4.Asynchronous JS/Promises/index.js
+++ JS_other_concepts/4.Asynchronous JS/Promises/index.js
@@ -1,6 +1,41 @@
-const prom=new Promise((resolve,reject)=>{
-        setTimeout(()=>{
-            resolve({user:"ed"});
-        },2000);
-        console.log("hello");
-});
+console.log( "Start" );
+
+function user(resolve, reject) {
+    setTimeout(() => {
+        resolve({"user":"naba93980","password":12345});
+    }, 2000);
+}
+function getUserVideos(user) {
+    console.log( user );
+    return new Promise((resolve, reject)=>
+    {
+        setTimeout(() => {
+        resolve (["video", "video2", "video3"]);
+    }, 2000);
+    })
+}
+function getvideoDetails(video) {
+    console.log( video );
+    
+    return new Promise((resolve,reject)=>{setTimeout(()=>{
+        resolve({"name":"dhinkachika","duration":"11:23 minutes"})
+    },2000)})
+    
+}
+
+
+const prom = new Promise(user);
+prom.then((userDetail) => {
+    console.log(userDetail);
+    return getUserVideos(userDetail.user);
+}).then((videos) => {
+    console.log(videos);
+    return getvideoDetails(videos[0]);
+}).then((videoDetail) => {
+    console.log(videoDetail);    
+})
+
+
+
+console.log("End");
+
