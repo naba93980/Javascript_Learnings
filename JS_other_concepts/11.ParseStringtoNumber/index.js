c59e0d40ad4675f0a489169a6ee239dbcabd3f2b
naba93980 <72063612+naba93980@users.noreply.github.com>
2022-09-03 00:14:24 +0530

    updated

---
+++ JS_other_concepts/11.ParseStringtoNumber/index.js
@@ -0,0 +1,11 @@
+var x = "1234";
+
+var usingConstructor = Number(x);
+console.log(usingConstructor);
+
+var usingParseInt = parseInt(x);
+console.log(usingParseInt);
+
+var usingPlus = +x;
+console.log(usingPlus);
+
