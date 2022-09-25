2a10e2f1d659a6fdf066fce5ec22aae5d0c888b9
naba93980 <72063612+naba93980@users.noreply.github.com>
2022-09-25 13:46:52 +0530

    added definition of spread syntax

--- JS_Thapa_technical/8.ES6_rest_spread_objectpropryies_destructuring/index.js
+++ JS_Thapa_technical/8.ES6_rest_spread_objectpropryies_destructuring/index.js
@@ -91,9 +91,9 @@ console.log( other );
 
 //different variable name in object destructuring
 let { namee: myNamee, age: myAgee, breed: myBreed } = obj;
-console.log(myNamee, myAgee, myBreed);//new names 
+console.log(myNamee, myAgee, myBreed);//new names
 
 
-
+//Spread syntax (...) allows an iterable, such as an array or string, to be expanded in places where zero or more arguments (for function calls) or elements (for array literals) are expected. In an object literal, the spread syntax enumerates the properties of an object and adds the key-value pairs to the object being created.
 
 
