bab3e69d47ccd555d9040c985aaeca22ecda5e45
naba93980 <72063612+naba93980@users.noreply.github.com>
2022-09-26 00:55:05 +0530

    added  js code to delete cookies

--- JS_other_concepts/13.Storage&Cookies/index.js
+++ JS_other_concepts/13.Storage&Cookies/index.js
@@ -11,4 +11,14 @@ console.log(localStorage.key(0));
 
 //sessionstorage is same as localstorage, just that it is valid for a particular tab
 
-document.cookie = "firstname=naba; max-age=3600; path=/;"
+document.cookie = "firstname=naba; max-age=3600;"
+document.cookie = "lastname=modak; max-age=3600;"
+
+//delete cookies using js:
+var allCookies = document.cookie.split('; ');
+for (let i = 0; i < allCookies.length; i++){
+    let cookie = allCookies[i];
+    let eqPos = cookie.indexOf("=");
+    let namee = eqPos > -1 ? cookie.substring(0, eqPos) : cookie;
+    document.cookie = namee + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT; path=/";
+}
